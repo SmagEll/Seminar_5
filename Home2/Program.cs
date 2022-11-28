fb44fe325600b37882cb08eee9a82e9ad28512b7
Elena.Smagina <esmagina17@gmail.com>
2022-11-28 20:16:37 +0500

    Home2

---
+++ Home2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int size = 10;
+int[] num = new int[size];
+FillArrayRandomNumbers(num);
+PrintArray(num);
+
+int sumNumInd = 0;
+
+for (int i = 1; i < num.Length; i += 2)
+{
+    sumNumInd += num[i];
+}
+Console.Write(sumNumInd);
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
